6c6adc422dca55fc998a4b5ed4573c8b559f6d2b
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2020-11-26 11:44:26 -0300

    header nao ta funcionando :exploding_head:

---
+++ src/tests/Header.test.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import Header from '../components/Header';
+// import RecipesProvider from '../provider/RecipesProvider';
+
+// test('Teste SearchBar', () => {
+//   const { getByText } = render(
+//     <RecipesProvider>
+//       <Header />
+//     </RecipesProvider>,
+//   );
+//   const profileTopBtn = screen.getByTestId('profile-top-btn');
+//   const searchtopBtn = screen.getByTestId('search-top-btn');
+//   expect(profileTopBtn).toBeInTheDocument();
+//   expect(searchtopBtn).toBeInTheDocument();
+// });
